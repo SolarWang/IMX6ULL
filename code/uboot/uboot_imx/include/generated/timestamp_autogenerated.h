36ad9e9c8a5c7d3773c9cf2196c54988cc0530f2
SolarWang <3540816677@qq.com>
2019-12-23 14:44:57 +0800

    add

--- code/uboot/uboot_imx/include/generated/timestamp_autogenerated.h
+++
@@ -1,4 +0,0 @@
-#define U_BOOT_DATE "Dec 23 2019"
-#define U_BOOT_TIME "10:49:53"
-#define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "12/23/2019"
--- code/uboot/uboot_imx/output/source/make.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-make distclean
-make mx6ull_14x14_ddr512_emmc_defconfig
-make V=1 -j12
