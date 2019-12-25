ce5d740b1e5cf63ece7ee54bd9deee774efeb406
solar <354081667@qq.com>
2019-12-25 17:10:36 +0800

    add

--- code/uboot/uboot-alientek/include/generated/timestamp_autogenerated.h
+++ code/uboot/uboot-alientek/include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Dec 23 2019"
-#define U_BOOT_TIME "15:52:49"
+#define U_BOOT_DATE "Dec 25 2019"
+#define U_BOOT_TIME "16:07:26"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "12/23/2019"
+#define U_BOOT_DMI_DATE "12/25/2019"
